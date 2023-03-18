5a6a8f2bda20d915adb8933adc535e53be45555c
Christian Hernandez <chernandez148@DESKTOP-3F6UKNT.localdomain>
2023-03-18 12:07:46 -0700

    update

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n%2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+
+
+# def make_exclamation(sentence_list):
+#     every_word = [(words + "!") for words in sentence_list]
+#     return every_word
+# make_exclamation(["I like computers", "I require coffee", "Live long and prosper"])
